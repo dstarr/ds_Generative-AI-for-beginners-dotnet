014e385c3e8ed02ed391315efa897050d2c89734
David Starr <david@elegantcode.com>
2025-03-13 12:25:40 -0700

    Passing setup code.

--- 02-SetupDevEnvironment/src/BasicChat-01MEAI/Program.cs
+++ 02-SetupDevEnvironment/src/BasicChat-01MEAI/Program.cs
@@ -1,12 +1,19 @@
-﻿using Azure;
-using Azure.AI.Inference;
+﻿using Azure.AI.OpenAI;
 using Microsoft.Extensions.AI;
+using System.ClientModel;
 
-IChatClient client = new ChatCompletionsClient(
-        endpoint: new Uri("https://models.inference.ai.azure.com"),
-        new AzureKeyCredential(Environment.GetEnvironmentVariable("GITHUB_TOKEN") ?? throw new InvalidOperationException("Missing GITHUB_TOKEN environment variable. Ensure you followed the instructions to setup a GitHub Token to use GitHub Models.")))
-        .AsChatClient("Phi-3.5-MoE-instruct");
+var azureAiSecret = Environment.GetEnvironmentVariable("AZURE_AI_SECRET") ?? throw new InvalidOperationException("Missing AZURE_AI_SECRET environment variable.");
+var azureAiEndpoint = Environment.GetEnvironmentVariable("AZURE_AI_ENDPOINT") ?? throw new InvalidOperationException("Missing AZURE_AI_ENDPOINT environment variable.");
+var azureAiModel = Environment.GetEnvironmentVariable("AZURE_AI_MODEL") ?? throw new InvalidOperationException("Missing AZURE_AI_MODEL environment variable.");
 
-var response = await client.GetResponseAsync("What is AI?");
+var endpoint = new Uri(azureAiEndpoint); // e.g. "https://< your hub name >.openai.azure.com/"
+var apiKey = new ApiKeyCredential(azureAiSecret);
+
+IChatClient client = new AzureOpenAIClient(
+    endpoint,
+    apiKey)
+.AsChatClient(azureAiModel);
+
+var response = await client.GetResponseAsync("What is AI? Answer in one sentence.");
 
 Console.WriteLine(response.Message);
